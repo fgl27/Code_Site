1219ba3f8ff8d10e9cf562ad2b76b649f9abcd30
Felipe Leon <fglfgl27@gmail.com>
2022-02-04 09:55:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 Start();
 
 function Start() {
-    if (document.readyState === "loading")
-        document.addEventListener("DOMContentLoaded", ProcessStart);
+    if (document.readyState === 'loading')
+        document.addEventListener('DOMContentLoaded', ProcessStart);
     else ProcessStart(); // `DOMContentLoaded` already fired
 }
 
 function ProcessStart() {
-    console.log("ProcessStart");
+    console.log('ProcessStart');
     let pageUrl = window.location.href;
-    if (pageUrl.indexOf("code=") !== -1) processCode(pageUrl);
+    if (pageUrl.indexOf('code=') !== -1) processCode(pageUrl);
     else showNoCode();
 }
 
 function showNoCode() {
-    document.getElementById("no_code").classList.remove("hidden");
+    document.getElementById('no_code').classList.remove('hidden');
 }
 
 function processCode(pageUrl) {
-    console.log("processCode");
-    code = pageUrl.match(/code=(\w+)/);
+    console.log('processCode');
+    const code = pageUrl.split('code=');
     console.log(code);
     if (code) {
-        code = code[1];
-        document.getElementById("code").classList.remove("hidden");
-        document.getElementById("code").innerHTML = code;
-        
+        const final_code = decodeURIComponent(code[1]);
+        console.log(final_code);
+        document.getElementById('code').classList.remove('hidden');
+        document.getElementById('code').innerHTML = final_code;
+
     } else {
         showNoCode();
     }
 }
+
+// https://login.salesforce.com/services/oauth2/authorize?
+// client_id=Consumer Key&
+// redirect_uri=Callback URL&
+// response_type=code
+
